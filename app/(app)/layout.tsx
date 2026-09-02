8cae9acfc663c5d3bfa3c85f25ec896a5bf4a314
hhakanguner-droid <hhakanguner@gmail.com>
2026-09-02 13:14:19 +0300

    feat: wrap product routes in ARVEN shell

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,5 @@
+import { AppShell } from "@/components/layout/AppShell";
+
+export default function ProductLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
+}
